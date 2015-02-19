680b69f8c5bb051afe084098fee61ed816d372b7
root <root@localhost.localdomain>
2015-02-19 10:05:23 -0500

    DLL Usage Files

---
+++ test.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+int test()
+{
+	MessageBox(NULL, "Hello, world!", "Hello, world!", MB_OK);
+	return 0;
+}
+
---
+++ test2.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+int test()
+{
+	MessageBox(NULL, "Hello, world again!", "Hello, world again!", MB_OK);
+	return 0;
+}
+
